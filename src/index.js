6d9db2bb83b62f2fef946ef080fa23131566b725
Nikita Yablochnikov <yablochnikovnikita@gmail.com>
2022-08-19 21:41:08 +0300

    simple counter using redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { legacy_createStore } from "redux";
 
-ReactDOM.render(
+console.log("Hello redux!");
+
+const root = createRoot(document.getElementById("root"));
+
+const initialState = { value: 0 };
+
+const reducer = (state = initialState, action) => {
+  console.log(action.payload);
+  switch (action.type) {
+    case "INC":
+      return { ...state, value: state.value + 1 };
+
+    case "DEC":
+      return { ...state, value: state.value - 1 };
+
+    case "RND":
+      return { ...state, value: state.value * action.payload };
+
+    default:
+      return state;
+  }
+};
+
+const store = legacy_createStore(reducer);
+
+const update = () => {
+  document.querySelector("#counter").textContent = store.getState().value;
+};
+
+store.subscribe(update);
+
+const inc = () => ({ type: "INC" });
+
+const dec = () => ({ type: "DEC" });
+
+const rnd = value => ({
+  type: "RND",
+  payload: value
+});
+
+document.getElementById("INC").addEventListener("click", () => {
+  store.dispatch(inc());
+});
+
+document.getElementById("DEC").addEventListener("click", () => {
+  store.dispatch(dec());
+});
+
+document.getElementById("RND").addEventListener("click", () => {
+  const value = Math.floor(Math.random() * (50 + 50) - 50);
+  store.dispatch(rnd(value));
+});
+
+root.render(
   <React.StrictMode>
-    <>
-    
-    </>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <></>
+  </React.StrictMode>
 );
